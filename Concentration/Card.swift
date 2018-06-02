f6dca59dac3040c1e1640206d381c790713b3393
パプリカ <yongtao.in@gmail.com>
2018-06-01 19:30:36 -0700

    minor fixes

--- Concentration/Card.swift
+++ Concentration/Card.swift
@@ -8,18 +8,24 @@
 
 import Foundation
 
-struct Card {
+struct Card: Hashable {
+    var hashValue: Int { return identifier }
+
+    static func == (lhs: Card, rhs: Card) -> Bool {
+        return lhs.identifier == rhs.identifier
+    }
+
     var isFaceUp = false
     var isMatched = false
-    var identifier: Int
-    
-    static var identifierFactory = 0
-    
-    fileprivate static func getUniqueIdentifier() -> Int {
+    private var identifier: Int
+
+    private static var identifierFactory = 0
+
+    private static func getUniqueIdentifier() -> Int {
         identifierFactory += 1
         return identifierFactory
     }
-    
+
     init() {
         self.identifier = Card.getUniqueIdentifier()
     }
